47b9daa0df5a467be67c28ad5f4e07e00fbc5ae4
Abhishek thakur <thakur11abhishek22@gmail.com>
2023-09-03 20:18:21 +0530

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 
 import { Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './Components/Footer'
 
 function App() {
   
 
   return (
     <>
-     <Routes>
-         
-     </Routes>
+     <Footer />
     </>
   )
 }
